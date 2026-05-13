ede3913fcea1c08097989392a7d97cbe68c8b93f
Amrit-Chand7 <chandamrit62@gmail.com>
2026-05-13 22:20:52 +0545

    Added Splash Screen

---
+++ app/src/main/java/com/example/drivesafe/SplashActivity.kt
@@ -0,0 +1,92 @@
+package com.example.drivesafe
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.drivesafe.ui.theme.DriveSafeTheme
+import kotlinx.coroutines.delay
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SplashBody()
+        }
+    }
+}
+
+@Composable
+fun SplashBody() {
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    LaunchedEffect(Unit) {
+
+        delay(3000)
+        val sharedPreferences = context.getSharedPreferences(
+            "User",
+            Context.MODE_PRIVATE
+        )
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn",false)
+        if(isLoggedIn){
+
+        }else{
+
+        }
+
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().background(
+            brush = Brush.linearGradient(
+                colors = listOf(
+                    // TOP
+                    Color(0xFFF9FAFB),
+                    // MIDDLE
+                    Color(0xFFEAFBF3),
+                    // BOTTOM
+                    Color(0xFFD4F5E9)
+
+                )
+            )
+        ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.logo_main),
+            contentDescription = null,
+            modifier = Modifier.size(400.dp)
+
+        )
+
+        CircularProgressIndicator()
+
+    }
+}
+
