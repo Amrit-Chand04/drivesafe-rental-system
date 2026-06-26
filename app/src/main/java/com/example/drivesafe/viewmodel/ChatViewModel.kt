e0f45e7e28c7daf950cc2d82ecc99811f0aed78a
prajeetajoshi9 <prajeetajoshi4@gmail.com>
2026-06-26 18:39:11 +0545

    ChatViewModel added

--- app/src/main/java/com/example/drivesafe/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/example/drivesafe/viewmodel/ChatViewModel.kt
@@ -1,4 +1,47 @@
 package com.example.drivesafe.viewmodel
 
-class ChatViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.drivesafe.model.ChatListModel
+import com.example.drivesafe.model.MessageModel
+import com.example.drivesafe.repo.ChatRepo
+
+class ChatViewModel(
+    private val repo: ChatRepo
+) : ViewModel() {
+
+    fun sendMessage(
+        chatId: String,
+        userId: String,
+        message: MessageModel,
+        chatList: ChatListModel,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.sendMessage(chatId, userId, message, chatList, callback)
+    }
+
+    fun getMessages(
+        chatId: String,
+        callback: (List<MessageModel>) -> Unit
+    ) {
+        repo.getMessages(chatId, callback)
+    }
+
+    fun getChatList(callback: (List<ChatListModel>) -> Unit) {
+        repo.getChatList(callback)
+    }
+
+    fun getUserChatPreview(
+        userId: String,
+        callback: (ChatListModel?) -> Unit
+    ) {
+        repo.getUserChatPreview(userId, callback)
+    }
+
+    fun markAdminChatAsRead(userId: String) {
+        repo.markAdminChatAsRead(userId)
+    }
+
+    fun markUserChatAsRead(userId: String) {
+        repo.markUserChatAsRead(userId)
+    }
 }
