87c732e4854fed9b28d516fda545dd3f220e57b7
prajeetajoshi9 <prajeetajoshi4@gmail.com>
2026-06-26 18:40:38 +0545

    ChatRepo added

--- app/src/main/java/com/example/drivesafe/repo/ChatRepo.kt
+++ app/src/main/java/com/example/drivesafe/repo/ChatRepo.kt
@@ -1,4 +1,34 @@
 package com.example.drivesafe.repo
 
+import com.example.drivesafe.model.ChatListModel
+import com.example.drivesafe.model.MessageModel
+
 interface ChatRepo {
+
+    fun sendMessage(
+        chatId: String,
+        userId: String,
+        message: MessageModel,
+        chatList: ChatListModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun getMessages(
+        chatId: String,
+        callback: (List<MessageModel>) -> Unit
+    )
+
+    fun getChatList(
+        callback: (List<ChatListModel>) -> Unit
+    )
+
+    fun getUserChatPreview(
+        userId: String,
+        callback: (ChatListModel?) -> Unit
+    )
+
+    fun markAdminChatAsRead(userId: String)
+
+    fun markUserChatAsRead(userId: String)
 }
+
