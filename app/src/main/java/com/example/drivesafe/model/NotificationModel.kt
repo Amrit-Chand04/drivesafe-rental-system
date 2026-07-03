a6c98ced9e24d4f78bc9c60c42f7a8e2a5e6fc5a
Amrit-Chand7 <chandamrit62@gmail.com>
2026-07-03 13:02:10 +0545

    Update the ui of Add vehicle and Edit Vehicle Alert box

---
+++ app/src/main/java/com/example/drivesafe/model/NotificationModel.kt
@@ -0,0 +1,8 @@
+package com.example.drivesafe.model
+
+data class NotificationModel(
+    var id: String = "",
+    var title: String = "",
+    var message: String = "",
+    var createdAt: Long = 0L
+)
